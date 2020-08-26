903d82ab2a12c16577953f8c1d1689294c8a395c
unknown <work.planetdigit@gmail.com>
2020-08-26 22:02:07 +0530

    new file

---
+++ live/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/live/admin/');
+define('HTTP_CATALOG', 'http://localhost/live/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/live/admin/');
+define('HTTPS_CATALOG', 'http://localhost/live/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/xampp/htdocs/live/admin/');
+define('DIR_SYSTEM', 'C:/xampp/htdocs/live/system/');
+define('DIR_IMAGE', 'C:/xampp/htdocs/live/image/');
+define('DIR_STORAGE', 'C:/xampp/live-storage/');
+define('DIR_CATALOG', 'C:/xampp/htdocs/live/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'live-db');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
